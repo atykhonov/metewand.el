505a2cf6232a3c638b892b6007ae13237481564f
Andrey Tykhonov <atykhonov@gmail.com>
2015-05-26 23:40:05 +0300

    Syntax highlighting and buffer evaluation.

--- metewand.el
+++ metewand.el
@@ -1,3 +1,38 @@
+(require 'f)
+
+(defvar boron-path (f-dirname (f-this-file)))
+
+(defvar boron-font-lock-keywords
+  ;; Keywords
+  `((,(rx symbol-start
+          (group (or
+                  (group "M-" (any print))
+                  "M-"
+                  "RET" "SPC" (group "C-" (any print)) "as" "elif" "global" "or" "with"
+                  "assert" "else" "if" "pass" "yield" "break" "except" "import" "class"
+                  "in" "raise" "continue" "finally" "is" "return" "def" "for" "lambda"
+                  "try"
+                  ;; Python 2:
+                  "print" "exec"
+                  ;; Python 3:
+                  ;; False, None, and True are listed as keywords on the Python 3
+                  ;; documentation, but since they also qualify as constants they are
+                  ;; fontified like that in order to keep font-lock consistent between
+                  ;; Python versions.
+                  "nonlocal"
+                  ;; Extra:
+                  "self"))
+          symbol-end)
+     (1 font-lock-function-name-face))
+    ;; Constants
+    (,(rx symbol-start
+          (or
+           "Ellipsis" "False" "None" "NotImplemented" "True" "__debug__"
+           ;; copyright, license, credits, quit and exit are added by the site
+           ;; module and they are not intended to be used in programs
+           "copyright" "credits" "exit" "license" "quit")
+          symbol-end) . font-lock-constant-face)))
+
 (defvar metewand-special-chars '("" "" "" "" "")
   "Special characters.")
 
@@ -6,6 +41,91 @@
   White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
   (replace-regexp-in-string "\\`[ \t\n\r]*" ""
                             (replace-regexp-in-string "[ \t\n\r]*\\'" "" string)))
+
+(defun boron-eval-current-buffer ()
+  (interactive)
+  (let ((win (selected-window))
+        (test-buffer (get-buffer-create "*test-buffer*"))
+        (curr-buffer (current-buffer))
+        (line nil)
+        (continue t)
+        (start nil)
+        (end nil)
+        (pos nil))
+    (with-current-buffer test-buffer
+      (erase-buffer))
+    (with-current-buffer curr-buffer
+      (goto-char (point-min))
+      (while (not (eobp))
+        (setq line (buffer-substring-no-properties
+                    (line-beginning-position)
+                    (line-end-position)))
+        (message "String: %s" (substring line 0 3))
+        (cond ((equal (substring line 0 3) "M-x")
+               (let ((begin nil)
+                     (end nil))
+                 (while (string-match "\\(\".+?\"\\)" line)
+                   (setq begin (match-beginning 1))
+                   (setq end (match-end 1))
+                   (setq line (concat
+                               (substring line 0 begin)
+                               (concat (apply (lambda (&rest args)
+                                                (let ((result ""))
+                                                  (dolist (arg args)
+                                                    (when (equal arg " ")
+                                                      (setq arg " SPC "))
+                                                    (setq result (concat result arg)))
+                                                  result))
+                                              (split-string (substring line (+ begin 1) (- end 1))
+                                                            "" t)))
+                               (substring line end))))))
+              ((equal (substring line 0 3) "M-:")
+               (setq line
+                     (concat
+                      "M-: "
+                      (apply (lambda (&rest args)
+                               (let ((result ""))
+                                 (dolist (arg args)
+                                   (when (equal arg " ")
+                                     (setq arg " SPC "))
+                                   (setq result (concat result arg)))
+                                 result))
+                             (split-string (substring line 4) "" t))))))
+        (when (not (equal (substring line 0 1)  "#"))
+          (unwind-protect
+              (with-current-buffer test-buffer
+                (set-window-buffer win test-buffer t)
+                (execute-kbd-macro
+                 (edmacro-parse-keys line)))
+            (set-window-buffer win curr-buffer t)))
+        (search-forward "\n" nil t)))))
+
+(defun boron-feature (text text2)
+  (interactive "sText: \nsText2: ")
+  (message "Feature: %s, %s" text text2))
+
+(defun boron-scenario (text)
+  (interactive "sText: ")
+  (message "Feature: %s" text))
+
+(defun boron-assert-equal (assertion)
+  (interactive "sEqual to: ")
+  (let ((test-buffer "*test-buffer*"))
+    (if (equal assertion
+               (with-current-buffer test-buffer
+                 (buffer-substring-no-properties (point-min) (point-max))))
+        (progn
+          (message "Test passed!")
+          (run-hook-with-args boron-reporter-scenario-passed-hook "Test feature"))
+      (progn
+        (message "Test failed!")
+        (run-hook-with-args boron-reporter-scenario-failed-hook "Test feature")))))
+
+(defun boron-insert (text)
+  (interactive "sText: ")
+  (let ((test-buffer "*test-buffer*"))
+    (with-current-buffer test-buffer
+      (insert text))))
 
 (defun metewand-eval-current-buffer ()
   (interactive)
@@ -122,4 +242,50 @@
     ;;   )
     ))
 
-(provide 'metewand)
+(defconst boron-syntax-propertize-function
+  (syntax-propertize-rules
+   ((rx "(.*)")
+    (0 (ignore (python-syntax-stringify))))))
+
+(defvar boron-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?# "<" table)
+    (modify-syntax-entry ?\n ">" table)
+    table)
+  "Syntax table.")
+
+;;;###autoload
+(define-derived-mode boron-mode fundamental-mode "Boron"
+  "Major mode for editing boron files.
+
+\\{boron-mode-map}"
+  ;; (set (make-local-variable 'comment-start-skip) "#+\\s-*")
+
+  (set (make-local-variable 'comment-start) "#")
+  ;; (set (make-local-variable 'comment-start-skip) "#+\\s-*")
+
+  (set (make-local-variable 'parse-sexp-lookup-properties) t)
+  (set (make-local-variable 'parse-sexp-ignore-comments) t)
+  
+  ;; (setq-local comment-start "#")
+  (setq-local comment-start-skip "#+ *")
+  ;; (setq-local comment-add 1)		;default to `;;' in comment-region
+  ;; (setq-local comment-column 40)
+  ;; (setq-local comment-use-syntax t)
+  
+  (set (make-local-variable 'font-lock-defaults)
+       '(boron-font-lock-keywords nil nil nil nil))
+
+  (set (make-local-variable 'syntax-propertize-function)
+       boron-syntax-propertize-function)
+
+  (set-syntax-table boron-syntax-table)
+
+  
+  ;; (set (make-local-variable 'paragraph-start) "\\s-*$")
+  ;; (set (make-local-variable 'fill-paragraph-function)
+  ;;      #'python-fill-paragraph)
+  
+  (set (make-local-variable 'paragraph-start) "\\s-*$"))
+
+(provide 'boron)
